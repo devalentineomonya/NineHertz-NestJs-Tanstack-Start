583197d7b99078eca99bc3a7cc55ac23852d2f33
Valentine Omonya <valomosh254@gmail.com>
2025-07-23 11:43:55 +0300

    added get and head endpoint

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get, Head } from '@nestjs/common';
+import { Public } from './auth/decorators/public.decorators';
 
 @Controller('app')
 export class AppController {
+  @Public()
   @Head()
   handleHeadRequest(): boolean {
     return true;
   }
+  @Public()
   @Get()
   handleGetRequest(): boolean {
     return true;
